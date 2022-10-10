7e205328f0a493829bf910232cda2ebaa7c381b8
Yasin Mamedov <mamedov.iakh@phystech.edu>
2022-10-10 14:54:02 +0300

    labels; trying to add named labels

--- asm.h
+++ asm.h
@@ -19,7 +19,7 @@ enum CMD
     CMD_HALT  = 11,
 };
 
-const int LABEL_AMOUNT = 50;
+const int LABEL_MAX = 50;
 
 struct Label 
 {
@@ -27,7 +27,7 @@ struct Label
     char *label_name; 
 };
 
-//#define DEBUG_MODE
+#define DEBUG_MODE
 
 #ifdef DEBUG_MODE
     #define DEB(...) printf(__VA_ARGS__)
@@ -35,16 +35,16 @@ struct Label
     #define DEB(...) 
 #endif// DEBUG_MODE
 
-#define ELSE_IF_CMP(CMND) else if(strincmp(cmd, #CMND, cmd_size) == 0) {machine_code_buffer[ip++] = CMD_##CMND; prev_push = 0;}
+#define ELSE_IF_CMP(CMND) else if(strincmp(cmd, #CMND, cmd_size) == 0) {machine_code_buffer[ip++] = CMD_##CMND; prev_push = 0; DEB("doing %s\n", #CMND);}
 
 FILE *WriteBinaryFile(int *machine_code_buffer, int machine_symbols_count);
 void OddSpaceRemoveArray(char *buffer);
+int strincmp(const char *str1, const char *str2, int n);
 char **GetString(char* buffer, int string_count);
 char *ReadToBuffer(const char *filename);
 int CountString(char *buffer);
-int strincmp(const char *str1, const char *str2, int n);
 int CountSymbols(const char *filename);
-int *Assembly(char **stringed_buffer, int strings_count, int *machine_symbols_count);
+int *Assemble(char **stringed_buffer, struct Label *labels, int strings_count, int *machine_symbols_count);
 
 
 #endif// ASM_H
--- casm.cpp
+++ casm.cpp
@@ -25,6 +25,34 @@ int CountSymbols(const char *filename)
     return buff.st_size;
 }
 
+int strincmp(const char *str1, const char *str2, int n)
+{
+    if (n == 0)
+    {
+        return -1;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        if(str1[i] == ' ' || str2[i] == ' ')
+        {
+            continue;
+        }
+        if(tolower(str1[i]) != tolower(str2[i]))
+        {
+           return str1[i] - str2[i];
+        }
+        if(str1[i] == '\0' && str2[i] != '\0') 
+        {
+            return -1;
+        }
+        if (str2[i] == '\0' && str1[i] != '\0')
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 char *ReadToBuffer(const char *filename)
 {
     int size = CountSymbols(filename);
@@ -90,21 +118,6 @@ int CountString(char *buffer)
     return count;
 }
 
-int strincmp(const char *str1, const char *str2, int n)
-{
-    for(int i = 0; i < n; i++)
-    {
-        int razn = tolower(str1[i]) - tolower(str2[i]);
-
-        if(razn)
-        {
-            return razn;
-        }
-    }
-    return 0;
-}
-
-
 char **GetString(char* buffer, const int string_count)
 {
     char **stringed_buffer = (char**)calloc(string_count, sizeof(char*));
@@ -120,25 +133,19 @@ char **GetString(char* buffer, const int string_count)
     return stringed_buffer;
 }
 
-int *Assembly(char **stringed_buffer, int strings_count, int *machine_symbols_count)
+int *Assemble(char **stringed_buffer, struct Label *labels, int strings_count, int *machine_symbols_count)
 {
     int *machine_code_buffer = (int*)calloc(2 * strings_count, sizeof(int)); // ocenka sverxu (cmd_amount* (max amount of args per cmd))
-    struct Label labels[LABEL_AMOUNT] = {};
-
-    for(int i = 0; i < LABEL_AMOUNT; i++)
-    {
-        labels[i].label_to   = -1;
-        labels[i].label_name = NULL;
-    }
 
     int ip = 0;
-    int prev_push = 0;
+    int prev_push = 0; //TODO: change name since it will be used for cmds with 2 args, not only push
 
     for(int i = 0; i < strings_count; i++)
     {
         char cmd[100] = {}; 
         int cmd_size = 0;
-        sscanf(stringed_buffer[i], "%s%n", cmd, &cmd_size);
+        int cmd_colon_size = 0;
+        char colon      = '?';
 
 //; comments
         char *semicolon	 = strchr(stringed_buffer[i], ';');
@@ -152,59 +159,81 @@ int *Assembly(char **stringed_buffer, int strings_count, int *machine_symbols_co
             }
         }
 
+        sscanf(stringed_buffer[i], "%[^:-123456789]%n%[:]%n", cmd, &cmd_size, &colon, &cmd_colon_size);
+
+        DEB("[%s / %d / %c / %d ]\n", cmd, cmd_size, colon, cmd_colon_size);
 // : labels 
-// TODO: label with names 
-        char *colon = strchr(stringed_buffer[i], ':');
-        int label_number = 0;
-        
-        if(colon != NULL) // if != NULL => something about labels
+// TODO: label with names   
+// ебаная хуйня cmd_size мб вообще юзать блять цифры а не сканфную переменную для длины? я же длину и так подчищаю... 
+// ^ пофиксит баг если джампить на именную : cmd_size(jmp next) == 8 => v strincmp jmp не зайдет 
+        if(strincmp(cmd, "jmp", cmd_size) == 0)
         {
-            if(strincmp(cmd, "jmp", cmd_size) == 0)
+            DEB("joined jmp with command : %s\n", cmd);
+            int jmp_to = 0;
+            
+            if(colon != ':')
             {
-                char *is_jmp_label = strchr(stringed_buffer[i], ':'); 
-                int jmp_to = 0;
-                
-                if(is_jmp_label == NULL)
-                {
-                    sscanf(stringed_buffer[i] + cmd_size + 1, " %d", &jmp_to);
-
-                    machine_code_buffer[ip++] = CMD_JMP;
-                    machine_code_buffer[ip++] = jmp_to;
-                }
-                else 
-                {
-                    sscanf(stringed_buffer[i] + cmd_size + 2, " %d", &jmp_to);
-
-                    machine_code_buffer[ip++] = CMD_JMP;
-                    machine_code_buffer[ip++] = labels[jmp_to].label_to;
-                }
+                sscanf(stringed_buffer[i] + cmd_size, " %d", &jmp_to);
+                DEB("[%s]colon != : %d\n", cmd, jmp_to);
+                machine_code_buffer[ip++] = CMD_JMP;
+                machine_code_buffer[ip++] = jmp_to;
             }
             else 
             {
-                if(sscanf(colon, ":%d", &label_number) == 1)
+                if(sscanf(stringed_buffer[i] + cmd_colon_size, " %d", &jmp_to))
                 {
-                    labels[label_number].label_to = ip;
+                DEB("colon ==  : %d\n", jmp_to);
+                machine_code_buffer[ip++] = CMD_JMP;
+                machine_code_buffer[ip++] = labels[jmp_to].label_to;
                 }
-                // HERE: else : for named labels
+                else 
+                {
+                    // sscanf(stringed_buffer, ":%s",);
+                }
             }
         }
-        else
-        { 
-        if(strincmp(cmd, "push", cmd_size) == 0)
+        else if(char *colonn = strchr(stringed_buffer[i], ':'))  // ну это хуйня блять сканфная не работает если строчка ":цифра"    
+        {
+            DEB("ebanaya metka : ");
+            int label_number = 0;
+            if(sscanf(colonn, ":%d", &label_number) == 1)
+            {
+                DEB("label_number = %d ; \n",label_number);
+                labels[label_number].label_to = ip;
+            }
+            else 
+            {
+                char *labels_name = cmd;
+                DEB("label name : %s\n", labels_name);
+                for(int i = 0; i < LABEL_MAX; i++)
+                {
+                    if(labels[i].label_name == NULL && labels[i].label_to == -1)
+                    {
+                        labels[i].label_to = ip;
+                        labels[i].label_name = labels_name;
+                    }
+                }
+            }
+        }
+
+        else if(strincmp(cmd, "push", cmd_size) == 0)
         {
             int push_value = 0;
-            sscanf(stringed_buffer[i] + cmd_size + 1, " %d", &push_value);
+            sscanf(stringed_buffer[i] + cmd_size, " %d", &push_value);
+            DEB("push value : %d\n", push_value);
             machine_code_buffer[ip++] = CMD_PUSH;
             machine_code_buffer[ip++] = push_value; 
             prev_push = 1;
         }
         else if(strincmp(cmd, "pop", cmd_size) == 0)
         {
+            DEB("popping\n");
             machine_code_buffer[ip++] = CMD_POP;
             prev_push = 0;
         }
         else if(strincmp(cmd, "dup", cmd_size) == 0)
         {
+            DEB("in dup..\n");
             if(prev_push == 1)
             {
                 machine_code_buffer[ip]     = CMD_PUSH;
@@ -223,8 +252,6 @@ int *Assembly(char **stringed_buffer, int strings_count, int *machine_symbols_co
         ELSE_IF_CMP(DIV)
         ELSE_IF_CMP(OUT)
         ELSE_IF_CMP(HALT)
-        }
-    
     }
     *machine_symbols_count = ip;
 
@@ -233,20 +260,26 @@ int *Assembly(char **stringed_buffer, int strings_count, int *machine_symbols_co
 
 
 
-int main() // TODO: if bebra.txt has odd empty stroki, the result of the program is unexpected
+int main() // TODO: if bebra.txt has empty string, the result of the program is unexpected???????? *(check again, ive might been drunk)
 {
     char *buffer = ReadToBuffer("bebra.txt");
     OddSpaceRemoveArray(buffer);
     int strings_count = CountString(buffer);
     char **string_buffer = GetString(buffer, strings_count);
 
-    for(int i = 0; i < strings_count; i++)
-    {
-        DEB("%s\n", string_buffer[i]);
-    }
     int machine_symbols_count = 0;
 
-    int *machined_buffer = Assembly(string_buffer, strings_count, &machine_symbols_count);
+    struct Label labels[LABEL_MAX] = {};
+
+    for(int i = 0; i < LABEL_MAX; i++)
+    {
+        labels[i].label_to   = -1;
+        labels[i].label_name = NULL;
+    }
+
+    int *machined_buffer = Assemble(string_buffer, labels, strings_count, &machine_symbols_count);
 
     WriteBinaryFile(machined_buffer, machine_symbols_count);
+   
+
 }
